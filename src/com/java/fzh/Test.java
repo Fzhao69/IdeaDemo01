4ade31c1ac8185733e744069bd974dda5bf32fdf
Fzihao <952715567@qq.com>
2019-07-23 13:04:12 +0800

    Initial commit

---
+++ src/com/java/fzh/Test.java
@@ -0,0 +1,57 @@
+package com.java.fzh;
+
+import java.util.ArrayList;
+
+/**
+ * @author fzhao
+ * Create 2019-07-22 16:06
+ * Description
+ */
+public class Test {
+    //    1.psvm
+    private static final int MAX_NUM = 1;
+    public static void main(String[] args) {
+        System.out.println("Test.main");
+        String[] arr = new String[]{"1","2","3"};
+        for (int i = 0; i < 10; i++) {
+
+        }
+        for (String s : arr) {
+            System.out.println(s);
+        }
+
+        ArrayList lis = new ArrayList();
+        lis.add(1);
+        lis.add(12);
+        lis.add(123);
+
+        for (Object li : lis) {
+
+        }
+        for (int i = 0; i < lis.size(); i++) {
+
+        }
+
+        for (int i = lis.size() - 1; i >= 0; i--) {
+
+        }
+
+        if (lis == null) {
+
+        }
+        if (lis != null) {
+
+        }
+        if (lis != null) {
+
+        }
+        if (lis == null) {
+
+        }
+
+    }
+
+    public void demo(){
+
+    }
+}
---
+++ src/com/java/fzh/helloword.java
@@ -0,0 +1,25 @@
+package com.java.fzh;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class helloword {
+    private static final int MAX_SIZE = 10;
+
+    public static void main(String[] args) {
+        System.out.println("HelloWord!!");
+
+        Date date = new Date();
+        System.out.println(date);
+
+        ArrayList list = new ArrayList();
+        list.add(0);
+    }
+
+    public void demo(){
+        int num = 10;
+        System.out.println("");
+    }
+
+
+}
